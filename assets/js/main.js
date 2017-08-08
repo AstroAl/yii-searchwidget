daeefa5481507cf5bb821b11412f7777ec277781
Biblbrox <belyabum@yandex.ru>
2017-08-09 02:51:00 +0300

    Simpify code

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,17 +4,12 @@ window.onload = function(event) {
     blockSearchBtn.setAttribute('disabled', true);
     document.getElementById('block-field').onkeyup = function() {
         let blockField = document.getElementById('block-field');
-        if(blockField.value !== 0 && /\S/.test(blockField.value))
-            blockSearchBtn.setAttribute('disabled', false);
-        else
-            blockSearchBtn.setAttribute('disabled', true);
+        blockSearchBtn.disabled = !(blockField.value !== 0 && /\S/.test(blockField.value));
     };
     navSearchBtn.setAttribute('disabled', true);
     document.getElementById('nav-field').onkeyup = function() {
-        let blockField = document.getElementById('nav-field');
-        if(blockField.value !== 0 && /\S/.test(blockField.value))
-            navSearchBtn.setAttribute('disabled', false);
-        else
-            navSearchBtn.setAttribute('disabled', true);
+        let navField = document.getElementById('nav-field');
+        navSearchBtn.disabled = !(navField.value !== 0 && /\S/.test(navField.value));
     };
 };
+
