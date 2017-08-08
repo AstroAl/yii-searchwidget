48e5e17065fece8f25192585ebf15fa38653f21c
Biblbrox <belyabum@yandex.ru>
2017-08-09 02:21:33 +0300

    Remove jquery

--- SearchAssets.php
+++ SearchAssets.php
@@ -8,7 +8,6 @@ class SearchAssets extends AssetBundle
     public $sourcePath = '@bibldev/searchwidget/assets';
 
     public $js = [
-        'js/jquery-3.2.1.min.js',
         'js/main.js'
     ];
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,20 @@
-$(document).ready(function() {
-    let blockSearchBtn = $('#block-search-btn');
-    blockSearchBtn.attr('disabled', true)
-    $('#block-field').keyup(function () {
-        if($(this).val().length !== 0 && /\S/.test($(this).val()))
-            blockSearchBtn.attr('disabled', false);
+window.onload = function(event) {
+    let blockSearchBtn = document.getElementById('block-search-btn'),
+        navSearchBtn = document.getElementById('nav-search-btn');
+    blockSearchBtn.setAttribute('disabled', true);
+    document.getElementById('block-field').onkeyup = function() {
+        let blockField = document.getElementById('block-field');
+        if(blockField.value !== 0 && /\S/.test(blockField.value))
+            blockSearchBtn.setAttribute('disabled', false);
         else
-            blockSearchBtn.attr('disabled', true);
-    });
-    let navSearchBtn = $('#nav-search-btn');
-    navSearchBtn.attr('disabled', true);
-    $('#nav-field').keyup(function () {
-        if($(this).val().length !== 0 && /\S/.test($(this).val()))
-            navSearchBtn.attr('disabled', false);
+            blockSearchBtn.setAttribute('disabled', true);
+    };
+    navSearchBtn.setAttribute('disabled', true);
+    document.getElementById('nav-field').onkeyup = function() {
+        let blockField = document.getElementById('nav-field');
+        if(blockField.value !== 0 && /\S/.test(blockField.value))
+            navSearchBtn.setAttribute('disabled', false);
         else
-            navSearchBtn.attr('disabled', true);
-    });
-});
+            navSearchBtn.setAttribute('disabled', true);
+    };
+};
